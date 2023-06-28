cf513bd7a64ea456025df1cea7096a0c96f05c9f
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-27 22:31:00 -0500

    sets up basic server.js file

---
+++ server.js
@@ -0,0 +1,22 @@
+// const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+// const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
+});
+
