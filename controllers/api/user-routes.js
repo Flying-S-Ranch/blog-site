25535fc1b60952efcd93e12f75fdb0a049fd7008
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-28 19:31:29 -0500

    adds post request for new users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
